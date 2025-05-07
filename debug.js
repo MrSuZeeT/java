cebff55e1f893cdb566030d600c989712c59c408
MrSuZeeT <sujitbasnetsb77@gmail.com>
2025-05-07 10:52:22 +0545

    Initial commit

--- debug.js
+++ debug.js
@@ -1,8 +1,7 @@
 
 const readline = require("readline");
 
-const rl = readline.createInterface({input: process.stdin,output: process.stdout
-});
+const rl = readline.createInterface({input: process.stdin,output: process.stdout});
 
 function std (){
     rl.question("Enter ur name: ", function(name){
@@ -14,7 +13,7 @@ function std (){
                 console.log("Value Unrecognized.Kindly enter Numeric value!");
             }
                 else if(age<18){
-                    console.log("Age restricted");
+                    console.log("Aint For u kiddo Get Out ASAP");
                 }
                 else{
             console.log("Your age is " + age);
--- java.js
+++
@@ -1,6 +0,0 @@
-if (number % 2 === 0) {
-    console.log(number + " is even");
-  } else {
-    console.log(number + " is odd");
-  }
-}
